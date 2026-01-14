0b900f81fb446aeb06430d76e07fac4f3c428d96
Omar <omar.garcia@morsa.com.mx>
2026-01-14 12:01:11 -0600

    Creando la PAGINA para CREAR proyectos

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "@/layouts/AppLayout";
 // import DashBoardView from "./views/DashBoardView";
 import DashBoardView from "@/views/DashBoardView";
+import CreateProjectView from "./views/projects/CreateProjectView";
 
 export default function Router() {
   return (
@@ -10,6 +11,7 @@ export default function Router() {
       <Routes>
         <Route element={<AppLayout />}>
           <Route path="/" element={<DashBoardView />} index />
+          <Route path="/projects/create" element={<CreateProjectView />} />
         </Route>
       </Routes>
     </BrowserRouter>
