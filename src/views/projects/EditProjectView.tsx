e069d5adc3fd90a8d99413dbb4349208b24ac458
Omar <omar.garcia@morsa.com.mx>
2026-01-14 22:06:42 -0600

    Obtener el proyecto a EDITAR

--- src/views/projects/EditProjectView.tsx
+++ src/views/projects/EditProjectView.tsx
@@ -1,3 +1,5 @@
+import { getProjectById } from "@/api/ProjectAPI";
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom"
 
 export default function EditProjectView() {
@@ -6,6 +8,13 @@ export default function EditProjectView() {
 
   const projectId = params.projectId!;
   // console.log(projectId);
+
+  const { data, isLoading, error, isError } = useQuery({
+    queryKey: ['editProject', projectId],
+    queryFn: () => getProjectById(projectId),
+    retry: false
+  });
+  console.log(data, isLoading, error, isError);
 
   return (
     <div>
