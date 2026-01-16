fdf9c92a7237449f06cd20b2e327168dc1a91b14
Omar <omar.garcia@morsa.com.mx>
2026-01-16 13:38:19 -0600

    Obteniendo la TAREA a EDITAR

--- src/api/TaskAPI.ts
+++ src/api/TaskAPI.ts
@@ -1,13 +1,17 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import type { Project, TaskFormData } from "../types";
+import type { Project, Task, TaskFormData } from "../types";
 
 type TaskAPI = {
-  formData: TaskFormData,
-  projectId: Project['_id']
+  formData: TaskFormData;
+  projectId: Project["_id"];
+  taskId: Task['_id'];
 };
 
-export async function createTask({formData, projectId}: Pick<TaskAPI, 'formData' | 'projectId'>) {
+export async function createTask({
+  formData,
+  projectId,
+}: Pick<TaskAPI, "formData" | "projectId">) {
   try {
     const url = `/projects/${projectId}/tasks`;
 
@@ -15,6 +19,22 @@ export async function createTask({formData, projectId}: Pick<TaskAPI, 'formData'
 
     return data;
   } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function getTaskById({ projectId, taskId }: Pick<TaskAPI, 'projectId' | 'taskId'>) {
+  try {
+    const url = `/projects/${projectId}/task/${taskId}`;
+
+    const { data } = await api(url);
+
+    return data;
+  } catch (error) {
+    // console.log(error);
+
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
     }
--- src/components/tasks/EditTaskData.tsx
+++ src/components/tasks/EditTaskData.tsx
@@ -1,14 +1,32 @@
-import { useLocation } from "react-router-dom"
+import { getTaskById } from "@/api/TaskAPI";
+import { useQuery } from "@tanstack/react-query";
+import { useLocation, useParams } from "react-router-dom"
 
 export default function EditTaskData() {
-  const location = useLocation();
+  // const location = useLocation();
   // console.log(location.search);
+
+  const params = useParams();
+  // console.log(params);
+
+  const projectId = params.projectId!;
+
+  const location = useLocation();
 
   const queryParams = new URLSearchParams(location.search);
   // console.log(queryParams);
 
-  const editTask = queryParams.get('editTask');
-  console.log(editTask);
+  const taskId = queryParams.get('editTask')!;
+  // console.log(taskId);
+
+  const { data } = useQuery({
+    queryKey: ['task', taskId],
+    queryFn: () => getTaskById({
+      projectId,
+      taskId
+    }),
+  });
+  console.log(data);
 
   return (
     <div>
