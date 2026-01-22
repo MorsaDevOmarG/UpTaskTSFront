6f9c69215d4bfa8689f8a178b4c04e937b98bc3c
Omar <omar.garcia@morsa.com.mx>
2026-01-22 00:15:42 -0600

    Creando la FUNCIONALIDAD de AUTENTICAR

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,5 +1,5 @@
 import { isAxiosError } from "axios";
-import type { ConfirmToken, RequestNewCodeView, UserRegistrationForm } from "../types";
+import type { ConfirmToken, RequestNewCodeView, UserLoginForm, UserRegistrationForm } from "../types";
 import api from "@/lib/axios";
 
 export async function createAccount(formData: UserRegistrationForm) {
@@ -31,6 +31,19 @@ export async function confirmAccount(formData: ConfirmToken) {
 export async function requestConfirmationCode(formData: RequestNewCodeView) {
   try {
     const url = '/auth/request-code';
+    const { data } = await api.post<string>(url, formData);
+
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function authenticateUser(formData: UserLoginForm) {
+  try {
+    const url = '/auth/login';
     const { data } = await api.post<string>(url, formData);
 
     return data;
