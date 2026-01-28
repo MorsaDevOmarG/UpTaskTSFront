bee364eb179caab916bd063d6e5c574a528577b5
Omar <omar.garcia@morsa.com.mx>
2026-01-27 21:24:06 -0600

    CReando el FORM para BUSCAR USUARIOS

--- src/views/projects/ProjectTeamView.tsx
+++ src/views/projects/ProjectTeamView.tsx
@@ -1,3 +1,4 @@
+import AddMemberModal from "@/components/team/AddMemberModal";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 export default function ProjectTeamView() {
@@ -30,6 +31,8 @@ export default function ProjectTeamView() {
           Volver a Proyecto
         </Link>
       </nav>
+
+      <AddMemberModal />
     </>
   );
 }
