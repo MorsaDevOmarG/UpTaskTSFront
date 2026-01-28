7fa7fce4db21b8ae317916e7c81a717aacb24104
Omar <omar.garcia@morsa.com.mx>
2026-01-27 21:14:20 -0600

    Creando las RUTAS

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -5,7 +5,7 @@ import EditTaskData from "@/components/tasks/EditTaskData";
 import TaskList from "@/components/tasks/TaskList";
 import TaskModalDetails from "@/components/tasks/TaskModalDetails";
 import { useQuery } from "@tanstack/react-query";
-import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
 
 export default function ProjectDetailsView() {
   const navigate = useNavigate();
@@ -34,21 +34,28 @@ export default function ProjectDetailsView() {
       <>
         <h1 className="text-5xl font-black">{data.projectName}</h1>
 
-        <p className="text-2xl font-light text-gray-500 mt-5">{data.description}</p>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          {data.description}
+        </p>
 
         <nav className="my-5 flex gap-3">
           <button
             type="button"
             className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-            onClick={() => navigate(location.pathname + '?newTask=true')}
+            onClick={() => navigate(location.pathname + "?newTask=true")}
           >
             Agregar Tarea
           </button>
+
+          <Link
+            to={"team"}
+            className="bg-fuchsia-600 hover:bg-fuchsia-700 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+          >
+            Colaboradores
+          </Link>
         </nav>
 
-        <TaskList
-          tasks={data.tasks}
-        />
+        <TaskList tasks={data.tasks} />
 
         <AddTaskModal />
 
