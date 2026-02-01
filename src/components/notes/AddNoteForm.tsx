932039970b4bca0905308c426f4cea4b491bf519
Omar <omar.garcia@morsa.com.mx>
2026-02-01 15:16:46 -0600

    Creando el TYPE del FORM para NOTAS

--- src/components/notes/AddNoteForm.tsx
+++ src/components/notes/AddNoteForm.tsx
@@ -1,24 +1,40 @@
-
+import type { NoteFormData } from "@/types/index";
+import { useForm } from "react-hook-form"
+import ErrorMessage from "../ErrorMessage";
 
 export default function AddNoteForm() {
+  const initialValues : NoteFormData = {
+    content: "",
+  };
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: initialValues,
+  });
+
+  const hanfleAddNote = (formData: NoteFormData) => {
+    console.log(formData);
+  };
+
   return (
-    <form
-      onSubmit={() => { }}
-      className="space-y-3"
-      noValidate
-    >
+    <form onSubmit={handleSubmit(hanfleAddNote)} className="space-y-3" noValidate>
       <div className="flex flex-col gap-2">
-        <label
-          htmlFor="content"
-          className="font-bold"
-        >Crear Nota</label>
+        <label htmlFor="content" className="font-bold">
+          Crear Nota
+        </label>
 
         <input
           type="text"
           id="content"
           placeholder="Contenido de la nota"
           className="w-full p-3 border border-gray-300"
+          {...register("content", {
+            required: "El contenido de la Nota es obligatorio",
+          })}
         />
+
+        {errors.content && (
+          <ErrorMessage>{errors.content.message}</ErrorMessage>
+        )}
       </div>
 
       <input
@@ -27,5 +43,5 @@ export default function AddNoteForm() {
         className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-2 text-white font-black cursor-pointer"
       />
     </form>
-  )
+  );
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -29,6 +29,17 @@ export const userSchema = authSchema.pick({
 export type User = z.infer<typeof userSchema>;
 export type UserProfileForm = Pick<User, "name" | "email">;
 
+// Notes
+export const noteSchema = z.object({
+  _id: z.string(),
+  content: z.string(),
+  createdBy: userSchema,
+  task: z.string(),
+});
+
+export type Note = z.infer<typeof noteSchema>;
+export type NoteFormData = Pick<Note, 'content'>;
+
 // Tasks
 export const taskStatusSchema = z.enum([
   "pending",
