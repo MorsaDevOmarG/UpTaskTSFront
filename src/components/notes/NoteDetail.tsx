159d5f9a19acab3d4587fac24e0ba03e9095dbe6
Omar <omar.garcia@morsa.com.mx>
2026-02-01 16:28:13 -0600

    Eliminando las NOTAS que creo el USUARIO

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,11 +1,27 @@
+import { useAuth } from "@/hooks/useAuth";
 import type { Note } from "@/types/index";
 import { formatDate } from "@/utils/utils";
+import { useMemo } from "react";
 
 type NoteDetailProps = {
   note: Note;
 };
 
 export default function NoteDetail({ note }: NoteDetailProps) {
+  const { data, isLoading } = useAuth();
+  console.log(data);
+
+  const canDelete = useMemo(
+    // () => data?._id === note.createdBy._id,
+    // Se agrego user para que evaluará correctamente el mostrar el botón de eliminar 
+    () => data?.user?._id === note.createdBy._id,
+    [data]
+  );
+
+  if (isLoading) {
+    return "Cargando...";
+  }
+
   return (
     <div className="p-3 flex justify-between items-center">
       <div>
@@ -14,10 +30,17 @@ export default function NoteDetail({ note }: NoteDetailProps) {
           <span className="font-bold">{note.createdBy.name}</span>
         </p>
 
-        <p className="text-xs text-slate-500">
-          {formatDate(note.createdAt)}
-        </p>
+        <p className="text-xs text-slate-500">{formatDate(note.createdAt)}</p>
       </div>
+
+      {canDelete && (
+        <button
+          type="button"
+          className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors"
+        >
+          Eliminar
+        </button>
+      )}
     </div>
   );
 }
