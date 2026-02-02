7f78e345b836b6d42aba25ccbc4a170c0d4e7a80
Omar <omar.garcia@morsa.com.mx>
2026-02-01 23:44:50 -0600

    Creando el FORM de PERFIL

--- src/views/profile/ProfileView.tsx
+++ src/views/profile/ProfileView.tsx
@@ -1,10 +1,12 @@
-
+import ProfileForm from "@/components/profile/ProfileForm";
+import { useAuth } from "@/hooks/useAuth"
 
 export default function ProfileView() {
+  const { data, isLoading } = useAuth();
 
-  return (
-    <div>
-      profile
-    </div>
-  )
+  if (isLoading) {
+    return 'Cargando...';
+  }
+
+  if (data) return  <ProfileForm data={data} />
 }
