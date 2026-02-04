f10fc8e1a50ed4805106911c00bbf640b46452d8
Omar <omar.garcia@morsa.com.mx>
2026-02-04 10:37:25 -0600

    Enviando la PETICION cuando SOLTAMOS, moviendo elementos DRAG & DROP

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,8 +1,12 @@
-import type { Task } from "@/types/index";
+import type { Task, TaskStatus } from "@/types/index";
 import TaskCard from "./TaskCard";
 import { statusTranslation } from "@/locales/es";
 import DropTask from "./DropTask";
 import { DndContext, DragEndEvent } from "@dnd-kit/core";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateStatus } from "@/api/TaskAPI";
+import { toast } from "react-toastify";
+import { useParams } from "react-router-dom";
 
 type TaskListProps = {
   tasks: Task[];
@@ -41,6 +45,28 @@ const statusStyles: { [key: string]: string } = {
 export default function TaskList({ tasks, cantEdit }: TaskListProps) {
   // console.log(tasks);
 
+  const params = useParams();
+  const projectId = params.projectId!;
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: updateStatus,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      toast.success(data);
+
+      queryClient.invalidateQueries({
+        queryKey: ["project", projectId],
+      });
+
+      queryClient.invalidateQueries({
+        // queryKey: ["task", taskId],
+      });
+    },
+  });
+
   const groupedTasks = tasks.reduce((acc, task) => {
     let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
     currentGroup = [...currentGroup, task];
@@ -54,9 +80,19 @@ export default function TaskList({ tasks, cantEdit }: TaskListProps) {
     const { over, active } = e;
 
     if (over && over.id) {
-      console.log("Valido");
-    } else {
-      console.log("Invalido");
+      // console.log("Valido", active.id);
+
+      const taskId = active.id.toString();
+
+      // console.log(over.id);
+
+      const status = over.id as TaskStatus;
+
+      mutate({
+        projectId,
+        taskId,
+        status,
+      });
     }
   };
 
