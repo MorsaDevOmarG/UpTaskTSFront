54fa2aecaf89238a62891887b7e32dc64da7133f
Omar <omar.garcia@morsa.com.mx>
2026-02-04 10:40:49 -0600

    Añadiendo efecto DRAG

--- src/components/tasks/DropTask.tsx
+++ src/components/tasks/DropTask.tsx
@@ -8,11 +8,17 @@ export default function DropTask({ status }: DropTaskProps) {
   const { isOver, setNodeRef } = useDroppable({
     id: status,
   });
+  // console.log(isOver);
+
+  const style = {
+    opacity: isOver ? 0.4 : undefined,
+  };
 
   return (
     <div
       className="text-xs font-semibold uppercase border border-dashed border-slate-500 mt-5 place-content-center text-slate-500"
       ref={setNodeRef}
+      style={style}
     >
       Soltar tarea aquí
     </div>
